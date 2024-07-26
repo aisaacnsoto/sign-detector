c0a5cada87c6bedf7fe3fd59b1a3f3cadf88cada
Alonso Novoa <aisaacnsoto@gmail.com>
2024-07-26 10:15:04 -0500

    agregando android

---
+++ android/app/src/main/java/com/aisaacnsoto/dev/MainActivity.java
@@ -0,0 +1,5 @@
+package com.aisaacnsoto.dev;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.aisaacnsoto.dev',
+  appName: 'Aprendiendo lenguaje de señas',
+  webDir: 'dist/signdetector'
+};
+
+export default config;
