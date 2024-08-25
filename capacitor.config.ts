ab939db5120f9f6f6d0078afb3d482195d89df79
Alonso Novoa <aisaacnsoto@gmail.com>
2024-08-25 16:44:33 -0500

    personalizacion de iconos y splash screen

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.aisaacnsoto.dev',
-  appName: 'Aprendiendo lenguaje de señas',
+  appName: 'Señas para todos',
   webDir: 'dist/signdetector',
   plugins: {
     SplashScreen: {
